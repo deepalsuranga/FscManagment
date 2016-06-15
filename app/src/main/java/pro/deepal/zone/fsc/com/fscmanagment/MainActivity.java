daa4c3ab66cdfb015eae197d133c1f22df77bfa2
deepalsuranga <deepalsuranga@live.com>
2016-06-15 10:50:40 +0530

    add component

--- app/src/main/java/pro/deepal/zone/fsc/com/fscmanagment/MainActivity.java
+++ app/src/main/java/pro/deepal/zone/fsc/com/fscmanagment/MainActivity.java
@@ -3,6 +3,8 @@ package pro.deepal.zone.fsc.com.fscmanagment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
     }
 }
